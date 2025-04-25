1055613c5eac7605c971bf8e2e9684bf738fa8ca
EstebanFerS <151755309+EstebanFerS@users.noreply.github.com>
2025-04-25 09:19:12 -0600

    Second Commit

---
+++ src/javaapplication7/so.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication7;
+import java.util.Random;
+
+/**
+ *
+ * @author esteb
+ */
+public class so {
+    public static void main(String[] args){
+    Random random = new Random();
+    
+    }
+}
